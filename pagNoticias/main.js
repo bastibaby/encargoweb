df6a4e845fb830f64e95d2b002f3b899f035bdf8
bastibaby <98473893+bastibaby@users.noreply.github.com>
2022-07-03 21:21:23 -0400

    Add files via upload

---
+++ pagNoticias/main.js
@@ -0,0 +1,8 @@
+document.querySelector(".menu-btn").addEventListener("click", () => {
+    document.querySelector(".nav-menu").classList.toggle("show")
+});
+
+ScrollReveal().reveal('.showcase');
+ScrollReveal().reveal('.cards-banner-one', { Delay: 500 });
+ScrollReveal().reveal('.cards-banner-two', { Delay: 500 });
+ScrollReveal().reveal('.newscards', { Delay: 500});
